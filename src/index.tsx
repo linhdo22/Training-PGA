c1fdfcfad907746169f5ca476b0a51fe9e9344d9
LinhDo <hoanglinhzed@gmail.com>
2022-02-16 23:05:33 +0700

    add scroll load, user detail, redirect login, redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 import IntlProviderWrapper from "./modules/intl/component/IntlProviderWrapper";
 import { createRootStore } from "./redux/configureStore";
 
-const { store } = createRootStore({});
+const { store, persistor } = createRootStore({});
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <IntlProviderWrapper>
-        <App />
-      </IntlProviderWrapper>
+      <PersistGate persistor={persistor} loading={"loading from persist"}>
+        <IntlProviderWrapper>
+          <App />
+        </IntlProviderWrapper>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
